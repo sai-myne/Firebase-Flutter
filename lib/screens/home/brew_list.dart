84df7b7f6b8fa31828ee3ae901f362dc56a1904a
sai-myne <ahlusir@gmail.com>
2020-07-27 13:32:14 +0800

    Brew Model

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
+import 'package:brew_crew/models/brew.dart';
 
 class BrewList extends StatefulWidget {
   @override
@@ -10,11 +10,12 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
-    // print(brews.documents);
-    for (var doc in brews.documents) {
-      print(doc.data);
-    }
+    final brews = Provider.of<List<Brew>>(context);
+    brews.forEach((brew) {
+      print(brew.name);
+      print(brew.sugars);
+      print(brew.strength);
+    });
     return Container();
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:brew_crew/models/brew.dart';
 
 class DatabaseService {
   final String uid;
@@ -15,8 +16,18 @@ class DatabaseService {
     });
   }
 
+  // brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) {
+      return Brew(
+          name: doc.data['name'] ?? '',
+          strength: doc.data['strength'] ?? 0,
+          sugars: doc.data['sugars'] ?? '0');
+    }).toList();
+  }
+
   // get brews stream
-  Stream<QuerySnapshot> get brews {
-    return brewCollection.snapshots();
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
   }
 }
