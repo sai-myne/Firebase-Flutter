a086810dcfe7e99089fad576dd94595daeb01dbf
sai-myne <ahlusir@gmail.com>
2020-07-27 09:16:51 +0800

    Anonymous Sign In

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -5,8 +5,7 @@ import 'package:brew_crew/screens/home/home.dart';
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // return either home or authenticate widget
-    return Authenticate();
+    return Home();
   }
 }
