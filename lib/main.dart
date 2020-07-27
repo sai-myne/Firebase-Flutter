b701be7a04cda5f6862b49017e1a06edb68a40fc
sai-myne <ahlusir@gmail.com>
2020-07-27 09:50:18 +0800

    Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'models/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,9 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
-
